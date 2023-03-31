41a79988aac388ed9f47cf296921e8ab0409dc8c
PromzzyKoncepts <okechukwupromise638@gmail.com>
2023-03-31 20:26:46 +0100

    modify components and add redux

---
+++ src/redux/messages/messages.js
@@ -0,0 +1,27 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+// Actions String
+const GET_MSG = 'get-messages';
+
+// Action Thunk
+export const retrieveMessages = createAsyncThunk(GET_MSG, async () => {
+  const res = await fetch('https://127.0.0.1:3000/api/v1/messages');
+  const { message } = await res.json();
+  return message;
+});
+
+// Reducers
+const messagesSlice = createSlice({
+  name: 'messages',
+  initialState: {
+    messages: [],
+  },
+  extraReducers: (builder) => {
+    builder.addCase(retrieveMessages.fulfilled, (state, action) => {
+      // eslint-disable-next-line no-param-reassign
+      state.messages[0] = action.payload;
+    });
+  },
+});
+
+export default messagesSlice.reducer;
