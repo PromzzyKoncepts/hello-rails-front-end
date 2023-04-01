552050c10b20ff1cac95f9733545cfd4c19b15f8
PromzzyKoncepts <okechukwupromise638@gmail.com>
2023-04-01 15:34:15 +0100

    add linters

--- src/redux/messages/messages.js
+++ src/redux/messages/messages.js
@@ -5,8 +5,8 @@ const GET_MSG = 'get-messages';
 
 // Action Thunk
 export const retrieveMessages = createAsyncThunk(GET_MSG, async () => {
-  const res = await fetch('https://127.0.0.1:3000/api/v1/messages');
-  const { message } = await res.json();
+  const res = await fetch('http://127.0.0.1:3000/api/v1/messages');
+  const message  = await res.json();
   return message;
 });
 
